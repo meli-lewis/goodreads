a65a5948f7d74baf4fd1425b5261b4721dee3f95
emmmbrown <ebrown118@gmail.com>
2015-01-12 19:21:54 -0800

    Added a transform of dates into Pandas datetime objects

--- goodreads_transform.py
+++ goodreads_transform.py
@@ -33,21 +33,37 @@ t1['ISBN'] = t1['ISBN'].str.replace('=', '')
 
 
 
-#make a new dataframe with just the ISBN and Bookshelves data
+# make a new dataframe with just the ISBN and Bookshelves data
 t2 = t1[['ISBN13','Bookshelves']]
 
 
+# check for nulls
+t2.isnull().sum()
+
+
+
+'''Check the formatting of the date'''
+b = t1.ix[0]['Date Added']
+print b
+type(b)
+
+# Format Date Added as pandas datetime, so it is easy to convert to other timestamp formats
+t1['Date Added'] = pd.to_datetime(t1['Date Added'] , format = '%Y/%m/%d')
+b = t1.ix[0]['Date Added']
+print b
+type(b)
+
+# Repeat for other date data
+t1['Date Read'] = pd.to_datetime(t1['Date Read'] , format = '%Y/%m/%d')
+t1['Original Purchase Date'] = pd.to_datetime(t1['Original Purchase Date'] , format = '%Y/%m/%d')
 
 t1
 
 
-# check for nulls
-t4.isnull().sum()
 
 
 t1.to_csv('full_table.csv')
 
 
-
-t4.to_csv('bookshelves.csv')
+t2.to_csv('bookshelves.csv')
 
